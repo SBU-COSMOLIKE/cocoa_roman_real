fed09847b5a0b2e8d0bbe586ac965a1f1adcf7dd
Vivian Miranda <vivian.miranda.astro@gmail.com>
2026-01-28 09:51:38 -0500

    Update

---
+++ EXAMPLE_EMUL_NAUTILUS2.py
@@ -0,0 +1,515 @@
+import warnings
+import os
+from sklearn.exceptions import InconsistentVersionWarning
+warnings.filterwarnings("ignore", category=InconsistentVersionWarning)
+warnings.filterwarnings(
+    "ignore",
+    message=".*column is deprecated.*",
+    module=r"sacc\.sacc"
+)
+warnings.filterwarnings(
+    "ignore",
+    category=RuntimeWarning,
+    message=r".*invalid value encountered*"
+)
+warnings.filterwarnings(
+    "ignore",
+    category=RuntimeWarning,
+    message=r".*overflow encountered*"
+)
+warnings.filterwarnings(
+    "ignore",
+    category=UserWarning,
+    message=r".*Function not smooth or differentiabl*"
+)
+warnings.filterwarnings(
+    "ignore",
+    category=UserWarning,
+    message=r".*Hartlap correction*"
+)
+import argparse, random
+import numpy as np
+from cobaya.yaml import yaml_load
+from cobaya.model import get_model
+from nautilus import Prior, Sampler
+from getdist import loadMCSamples
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+parser = argparse.ArgumentParser(prog='EXAMPLE_PROJECT_NAUTILUS1')
+parser.add_argument("--root",
+                    dest="root",
+                    help="Name of the Output File",
+                    nargs='?',
+                    const=1,
+                    default="./projects/lsst_y1/")
+parser.add_argument("--outroot",
+                    dest="outroot",
+                    help="Name of the Output File",
+                    nargs='?',
+                    const=1,
+                    default="example_nautilus1")
+parser.add_argument("--nlive",
+                    dest="nlive",
+                    help="Number of live points ",
+                    type=int,
+                    nargs='?',
+                    const=1,
+                    default=1000)
+parser.add_argument("--maxfeval",
+                    dest="maxfeval",
+                    help="Minimizer: maximum number of likelihood evaluations",
+                    type=int,
+                    nargs='?',
+                    const=1,
+                    default=100000)
+parser.add_argument("--neff",
+                    dest="neff",
+                    help="Minimum effective sample size. ",
+                    type=int,
+                    nargs='?',
+                    const=1,
+                    default=10000)
+parser.add_argument("--flive",
+                    dest="flive",
+                    help="Maximum fraction of the evidence contained in the live set before building the initial shells terminates",
+                    type=float,
+                    nargs='?',
+                    const=1,
+                    default=0.01)
+parser.add_argument("--nnetworks",
+                    dest="nnetworks",
+                    help="Number of Neural Networks",
+                    type=int,
+                    nargs='?',
+                    const=1,
+                    default=4)
+args, unknown = parser.parse_known_args()
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+yaml_string=r"""
+likelihood:
+  roman_real.cosmic_shear:
+    use_emulator: 1
+    path: ./external_modules/data/roman_real
+    data_file: example1.dataset # that assumes lens = source
+params:
+  As_1e9:
+    prior:
+      min: 0.5
+      max: 5
+    ref:
+      dist: norm
+      loc: 2.1
+      scale: 0.65
+    proposal: 0.4
+    latex: 10^9 A_\mathrm{s}
+    drop: true
+    renames: A
+  ns:
+    prior:
+      min: 0.87
+      max: 1.07
+    ref:
+      dist: norm
+      loc: 0.96605
+      scale: 0.01
+    proposal: 0.01
+    latex: n_\mathrm{s}
+  H0:
+    prior:
+      min: 55
+      max: 91
+    ref:
+      dist: norm
+      loc: 67.32
+      scale: 5
+    proposal: 3
+    latex: H_0
+  omegab:
+    prior:
+      min: 0.03
+      max: 0.07
+    ref:
+      dist: norm
+      loc: 0.0495
+      scale: 0.004
+    proposal: 0.004
+    latex: \Omega_\mathrm{b}
+    drop: true
+  omegam:
+    prior:
+      min: 0.1
+      max: 0.9
+    ref:
+      dist: norm
+      loc: 0.316
+      scale: 0.02
+    proposal: 0.02
+    latex: \Omega_\mathrm{m}
+    drop: true
+  w0pwa:
+    prior:
+      min: -5 
+      max: -0.01
+    ref:
+      dist: norm
+      loc: -0.99
+      scale: 0.05
+    proposal: 0.05
+    latex: w_{0,\mathrm{DE}}+w_{a,\mathrm{DE}}
+  w:
+    prior:
+      min: -3
+      max: -0.01
+    ref:
+      dist: norm
+      loc: -0.99
+      scale: 0.05
+    proposal: 0.05
+    latex: w_{0,\mathrm{DE}}
+  wa:
+    derived: 'lambda w0pwa, w: w0pwa - w'
+    latex: 'w_{a,\mathrm{DE}}'
+  As:
+    derived: 'lambda As_1e9: 1e-9 * As_1e9'
+    latex: A_\mathrm{s}
+  # ------------------------------------------------------------------------
+  # ------------------------------------------------------------------------
+  # Nuisance parameters below (it overwrites the default settings)
+  # ------------------------------------------------------------------------
+  # ------------------------------------------------------------------------
+  roman_DZ_S1:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    proposal: 0.002
+    latex: \Delta z_\mathrm{s,roman}^1
+  roman_DZ_S2:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    proposal: 0.002
+    latex: \Delta z_\mathrm{s,roman}^2
+  roman_DZ_S3:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    proposal: 0.002
+    latex: \Delta z_\mathrm{s,roman}^3
+  roman_DZ_S4:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    proposal: 0.002
+    latex: \Delta z_\mathrm{s,roman}^4
+  roman_DZ_S5:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    proposal: 0.002
+    latex: \Delta z_\mathrm{s,roman}^5
+  roman_DZ_S6:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    proposal: 0.002
+    latex: \Delta z_\mathrm{s,roman}^6
+  roman_DZ_S7:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    proposal: 0.002
+    latex: \Delta z_\mathrm{s,roman}^7
+  roman_DZ_S8:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.002
+    proposal: 0.002
+    latex: \Delta z_\mathrm{s,roman}^8
+  # ------------------------------------------------------------------------
+  # ------------------------------------------------------------------------
+  roman_M1:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    proposal: 0.005
+    latex: m_\mathrm{roman}^1
+  roman_M2:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    proposal: 0.005
+    latex: m_\mathrm{roman}^2
+  roman_M3:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    proposal: 0.005
+    latex: m_\mathrm{roman}^3
+  roman_M4:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    proposal: 0.005
+    latex: m_\mathrm{roman}^4
+  roman_M5:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    proposal: 0.005
+    latex: m_\mathrm{roman}^5
+  roman_M6:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    proposal: 0.005
+    latex: m_\mathrm{roman}^6
+  roman_M7:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    proposal: 0.005
+    latex: m_\mathrm{roman}^7
+  roman_M8:
+    prior:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    ref:
+      dist: norm
+      loc: 0.0
+      scale: 0.005
+    proposal: 0.005
+    latex: m_\mathrm{roman}^8
+  # ------------------------------------------------------------------------
+  # ------------------------------------------------------------------------
+  # Intrinsic alignment
+  roman_A1_1:
+    prior:
+      min: -5
+      max:  5
+    ref:
+      dist: norm
+      loc: 0.7
+      scale: 0.5
+    proposal: 0.5
+    latex: A_\mathrm{1-IA,roman}^1
+  roman_A1_2:
+    prior:
+      min: -5
+      max:  5
+    ref:
+      dist: norm
+      loc: -1.7
+      scale: 0.5
+    proposal: 0.5
+  roman_A2_1:
+    value: 0.0
+    latex: A_\mathrm{2-IA,Roman}^1
+  roman_A2_2:
+    value: 0.0
+    latex: A_\mathrm{2-IA,Roman}^2
+  roman_BTA_1:
+    value: 0.0
+    latex: A_\mathrm{BTA-IA,Roman}^1
+theory:
+  emul_cosmic_shear:
+    path: ./cobaya/cobaya/theories/
+    stop_at_error: True
+    extra_args: 
+      device: 'cuda'
+      file:  ['./projects/roman_real/emulators/w0wa_nla_halofit_cosmic_shear_cnn/roman_real_w0wa_hmcode2020_nobaryon_cs_CNN.pt']
+      extra: ['./projects/roman_real/emulators/w0wa_nla_halofit_cosmic_shear_cnn/roman_real_w0wa_hmcode2020_nobaryon_cs_CNN.h5']
+      ord: [['As_1e9','ns','H0','omegab', 'omegam', 'w', 'w0pwa',
+             'roman_DZ_S1','roman_DZ_S2','roman_DZ_S3','roman_DZ_S4',
+             'roman_DZ_S5','roman_DZ_S6','roman_DZ_S7','roman_DZ_S8',
+             'roman_A1_1','roman_A1_2']]
+      fast_params: [['roman_M1','roman_M2','roman_M3','roman_M4',
+                     'roman_M5','roman_M6','roman_M7','roman_M8']]
+      extrapar: [{'MLA': 'CNN', 
+                  'INT_DIM_RES': 256, 
+                  'CNN_DIM': 576, 
+                  'KERNEL_DIM': 21,
+                  'OUTPUT_DIM': 1080}]
+"""
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+model = get_model(yaml_load(yaml_string))
+def chi2(p):
+    p = [float(v) for v in p.values()] if isinstance(p, dict) else p
+    if np.any(np.isinf(p)) or  np.any(np.isnan(p)):
+      raise ValueError(f"At least one parameter value was infinite (CoCoa) param = {p}")
+    point = dict(zip(model.parameterization.sampled_params(), p))
+    res1 = model.logprior(point,make_finite=False)
+    if np.isinf(res1) or  np.any(np.isnan(res1)):
+      return 1.e20
+    res2 = model.loglike(point,
+                         make_finite=False,
+                         cached=False,
+                         return_derived=False)
+    if np.isinf(res2) or  np.any(np.isnan(res2)):
+      return 1e20
+    return -2.0*(res1+res2)
+
+def likelihood(params):
+  res = chi2(params)
+  if (res > 1.e19 or np.isinf(res) or  np.isnan(res)):
+    return -np.inf
+  else:
+    return -0.5*res
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+from mpi4py.futures import MPIPoolExecutor
+
+if __name__ == '__main__':
+    print(f"nlive={args.nlive}, output={args.root}chains/{args.outroot}")
+    # Build Nautilus Prior from Cobaya
+    NautilusPrior = Prior()                                       # Nautilus Call 
+    dim    = model.prior.d()                                      # Cobaya call
+    bounds = model.prior.bounds(confidence=0.999999)              # Cobaya call
+    names  = list(model.parameterization.sampled_params().keys()) # Cobaya Call
+    for b, name in zip(bounds, names):
+      NautilusPrior.add_parameter(name, dist=(b[0], b[1]))
+    
+    sampler = Sampler(NautilusPrior, 
+                      likelihood,  
+                      filepath=f"{args.root}chains/{args.outroot}_checkpoint.hdf5", 
+                      n_dim=dim,
+                      pool=MPIPoolExecutor(),
+                      n_live=args.nlive,
+                      n_networks=args.nnetworks,
+                      resume=True)
+    sampler.run(f_live=args.flive,
+                n_eff=args.neff,
+                n_like_max=args.maxfeval,
+                verbose=True,
+                discard_exploration=True)
+    points, log_w, log_l = sampler.posterior()
+    
+    # Save output file ---------------------------------------------------------
+    os.makedirs(os.path.dirname(f"{args.root}chains/"),exist_ok=True)
+    np.savetxt(f"{args.root}chains/{args.outroot}.1.txt",
+               np.column_stack((np.exp(log_w), log_l, points, -2*log_l)),
+               fmt="%.5e",
+               header=f"nlive={args.nlive}, maxfeval={args.maxfeval}, log-Z ={sampler.log_z}\n"+' '.join(names),
+               comments="# ")
+    
+    # Save a range files -------------------------------------------------------
+    rows = [(str(n),float(l),float(h)) for n,l,h in zip(names,bounds[:,0],bounds[:,1])]
+    with open(f"{args.root}chains/{args.outroot}.ranges", "w") as f: 
+      f.writelines(f"{n} {l:.5e} {h:.5e}\n" for n, l, h in rows)
+
+    # Save a paramname files ---------------------------------------------------
+    param_info = model.info()['params']
+    latex  = [param_info[x]['latex'] for x in names]
+    names.append("chi2*")
+    latex.append("\\chi^2")
+    np.savetxt(f"{args.root}chains/{args.outroot}.paramnames", 
+               np.column_stack((names,latex)),
+               fmt="%s")
+
+    # Save a cov matrix --------------------------------------------------------
+    samples = loadMCSamples(f"{args.root}chains/{args.outroot}",
+                            settings={'ignore_rows': u'0.0'})
+    np.savetxt(f"{args.root}chains/{args.outroot}.covmat",
+               np.array(samples.cov(), dtype='float64'),
+               fmt="%.5e",
+               header=' '.join(names),
+               comments="# ")
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
