8a122809e7a03a948d1ee35794d22092cc1c6fa2
Vivian Miranda <vivian.miranda@stonybrook.edu>
2026-02-12 19:45:49 -0500

    renamed files

--- EXAMPLE_EMUL_NAUTILUS1.py
+++ EXAMPLE_EMUL_NAUTILUS1.py
@@ -378,8 +378,8 @@ theory:
     stop_at_error: True
     extra_args: 
       device: 'cuda'
-      file:  ['./projects/roman_real/emulators/w0wa_nla_halofit_cosmic_shear_cnn/w0wa_takahashi_nobaryon_cs_CNN.pt']
-      extra: ['./projects/roman_real/emulators/w0wa_nla_halofit_cosmic_shear_cnn/w0wa_takahashi_nobaryon_cs_CNN.h5']
+      file:  ['./projects/roman_real/emulators/w0wa_nla_halofit_cosmic_shear/w0wa_takahashi_nobaryon_cs_CNN.pt']
+      extra: ['./projects/roman_real/emulators/w0wa_nla_halofit_cosmic_shear/w0wa_takahashi_nobaryon_cs_CNN.h5']
       ord: [['As_1e9','ns','H0','omegab', 'omegam', 'w', 'w0pwa',
              'roman_DZ_S1','roman_DZ_S2','roman_DZ_S3','roman_DZ_S4',
              'roman_DZ_S5','roman_DZ_S6','roman_DZ_S7','roman_DZ_S8',
